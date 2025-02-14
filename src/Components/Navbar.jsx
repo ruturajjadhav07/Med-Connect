af33a256a6abfa3df7712db573078c387428867e
“[github]” <ruturajjadhav122@gmail.com>
2025-02-14 13:00:17 +0530

    added features and icons

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/js/bootstrap.bundle.min";
 import logo from "/src/assets/logo.jpg";
 import Main from "./Main";
 const Navbar = () => {
