4eb3df52240ae6252e8fb956b5be9c47016d0c44
“[github]” <ruturajjadhav122@gmail.com>
2025-02-16 17:20:21 +0530

    search checked

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,22 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { useNavigate } from "react-router-dom";
 const Search = () => {
-  return (
-    // <div className="d-flex justify-content-center align-items-center flex-column mt-4">
-    //   <h1 className="text-success">Med Connect 💊</h1>
-    //   <form className="col-md-4 col-sm-8 col-10">
-    //     <input
-    //       type="text"
-    //       className="form-control text-center"
-    //       id="text"
-    //       placeholder="Enter Location near you"
-    //     />
-    //     <button className="btn btn-outline-success rounded mt-2 w-100">Search</button>
-    //   </form>
-    // </div>
+  const [search, setSearch] = useState("");
 
-    <div>
-      <h1 className="text-muted text-center mt-4" >Something Comming Soon</h1>
+  let navigate = useNavigate();
+  const submit = (e) => {
+    e.preventDefault();
+    navigate("/working");
+  };
+
+  return (
+    <div className="d-flex justify-content-center align-items-center flex-column mt-4">
+      <h1 className="text-success">Med Connect 💊</h1>
+      <form className="col-md-4 col-sm-8">
+        <input
+          type="text"
+          className="form-control text-center"
+          id="text"
+          placeholder="Enter Location near you"
+          onChange={(e) => {
+            setSearch(e.target.value);
+          }}
+          value={search}
+        />
+        <button
+          className="btn btn-outline-success rounded mt-2 w-100"
+          onClick={submit}
+        >
+          Search
+        </button>
+      </form>
     </div>
   );
 };
---
+++ src/Working.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const Working = () => {
+  return (
+    <div className="d-flex flex-column align-items-center justify-content-center vh-100">
+      <p className="text-secondary mt-2 fs-1">
+        Something exciting is coming soon!
+      </p>
+    </div>
+  );
+};
+
+export default Working;
